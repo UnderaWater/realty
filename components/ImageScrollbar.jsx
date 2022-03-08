cf884bc4b3161be11ac87d89edfacf8fb9c79d81
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-03-08 18:52:48 +0300

    fix: fixed arrows sroll in imagescrollbar

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -9,7 +9,7 @@ const LeftArrow = () => {
 
     return (
         <Flex justifyContent='center' alignItems='center' marginRight='1'>
-            <Icon as={FaArrowAltCircleLeft} onClick={scrollPrev} fontSize='2xl' cursor='pointer' />
+            <Icon as={FaArrowAltCircleLeft} onClick={() => scrollPrev()} fontSize='2xl' cursor='pointer' />
         </Flex>
     )
 }
@@ -19,27 +19,27 @@ const RightArrow = () => {
 
     return (
         <Flex justifyContent='center' alignItems='center' marginRight='1'>
-            <Icon as={FaArrowAltCircleRight} onClick={scrollNext} fontSize='2xl' cursor='pointer' />
+            <Icon as={FaArrowAltCircleRight} onClick={() => scrollNext()} fontSize='2xl' cursor='pointer' />
         </Flex>
     )
 }
 
-const ImageScrollbar = ({ data }) => {
+const ImageScrollbar = ({ data }) => (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }}>
         {data.map((item) => (
-            <Box key={item.id} width='910px' itemID={item.id} overflow='hidden' p='1'>
+            <Box key={item.id} width='910px' itemId={item.id} overflow='hidden' p='1'>
                 <Image
                     placeholder='blur'
-                    blurDataUrl={item.url}
+                    blurDataURL={item.url}
                     src={item.url}
                     width={1000}
                     height={500}
                     alt='property'
-                    sizes='(max-width:500px) 100px, (max-width):1024px 400px, 1000px'
+                    sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
                 />
             </Box>
         ))}
     </ScrollMenu>
-}
+)
 
 export default ImageScrollbar;
